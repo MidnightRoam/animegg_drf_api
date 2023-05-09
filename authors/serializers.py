780e7648b0a34da31a6f29c2768a1ce0c9f5ff1f
Uladzislau Saldatsenka <midnightroam1@gmail.com>
2023-05-09 19:20:43 +0200

    Optimized database queries for authors

--- authors/serializers.py
+++ authors/serializers.py
@@ -2,11 +2,13 @@ from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 
 from manga.serializers import MangaSerializer
-from .models import Author
+from .models import Author, Responsibility
+from manga.serializers import MangaSerializer
 
 
 class AuthorSerializer(ModelSerializer):
     """Author serializer class"""
+    responsibilities = serializers.SerializerMethodField()
 
     class Meta:
         model = Author
@@ -20,4 +22,20 @@ class AuthorSerializer(ModelSerializer):
             'image',
             'manga',
             'anime',
+        )
+
+    def get_responsibilities(self, obj):
+        """ORM query optimization for responsibilities field"""
+        responsibilities = obj.responsibilities.all()
+        serializer = ResponsibilitySerializer(responsibilities, many=True)
+        return serializer.data
+
+
+class ResponsibilitySerializer(ModelSerializer):
+    """Responsibility serializer class"""
+    class Meta:
+        model = Responsibility
+        fields = (
+            'id',
+            'title',
         )
